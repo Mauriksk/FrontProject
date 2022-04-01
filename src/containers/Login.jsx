afd9cc69fba8f2bdc11764b41cc214d56134f14a
Mauricio Torres <otarksk@gmail.com>
2022-04-01 14:24:50 -0300

    Usuarios guardados

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -3,19 +3,23 @@ import firebaseApp from '../firebase/credenciales'
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
 import { getFirestore, doc, setDoc } from 'firebase/firestore'
 const auth = getAuth(firebaseApp)
-const firestore = getFirestore(firebaseApp)
+
 
 export const Login = () => {
-
+    
     const [isRegistrando, setisRegistrando] = useState(false)
 
-
+    const firestore = getFirestore(firebaseApp)
     const registrarUsuario = async (email, password, rol)=>{
         const infoUsuario = await createUserWithEmailAndPassword(auth, email, password)
         .then( (usuarioFirebase) => usuarioFirebase )
 
         console.log(infoUsuario.user.uid)
-        const docuRef = doc(firebaseApp, `usuarios/${infoUsuario.user.uid}` )
+        const docuRef =  doc(firestore, `usuarios/${infoUsuario.user.uid}` )
+        setDoc(docuRef, {
+            correo: email,
+            rol: rol,
+        })
     }
 
 
