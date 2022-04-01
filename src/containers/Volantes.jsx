1084e9c3a53c83b83cd8ef4f1c42f2e7e1e4721f
Mauricio Torres <otarksk@gmail.com>
2022-04-01 19:15:52 -0300

    form agregado

--- src/containers/Volantes.jsx
+++ src/containers/Volantes.jsx
@@ -1,20 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Volantes = () => {
 
-    const [state, setState] = useState('')
+    const [formState, setFormState] = useState({
+        name:'',
+        gmail:''
+    })
+
+    const {name , gmail} = formState
+
+    useEffect(()=>{
+        console.log('hey')
+    }, [])
+
+    const handleInputChange = ({target})=>{
+        setFormState({
+            ...formState,
+            [target.name]:target.value
+        })
+    }
 
   return (
-    <div>
-        <h1>Volante</h1>
-
-
-        <label>Martillos</label>
-        <input 
-            type="number" 
-            name="martillo" 
-        />
+    <div className='container'>
+        <h1>Volantes</h1>
+            <hr/>
         
+        <div className='form-group'>
+                <input 
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Tu Nombre"
+                autoComplete="off"
+                value={name}
+                onChange={handleInputChange}
+                />
+            </div>
+
+            <div className='form-group'>
+                <input 
+                type="text"
+                name="gmail"
+                className="form-control"
+                placeholder="Gmail"
+                autoComplete="off"
+                value={gmail}
+                onChange={handleInputChange}
+                />
+            </div>
+
     </div>
   )
 }
